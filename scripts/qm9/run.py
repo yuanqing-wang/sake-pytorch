572332845c36ca7f2adad9bfae2dc0d60b1d9aeb
Yuanqing Wang <wangyq@wangyq.net>
2021-12-28 15:59:17 -0500

    qm9 script

--- scripts/qm9/run.py
+++ scripts/qm9/run.py
@@ -4,18 +4,8 @@ import dgl
 import sake
 from typing import Callable
 
-def padding(xs):
-    gs, ys = zip(*xs)
-    _zs = [g.ndata['Z'] for g in gs]
-    masks = [torch.ones_like(z) for z in _zs]
-    masks = torch.nn.utils.rnn.pad_sequence(masks, batch_first=True)
-    masks = masks.unsqueeze(-1) * masks.unsqueeze(-2)
-    zs = [torch.nn.functional.one_hot(z, 10) for z in _zs]
-    rs = [g.ndata['R'] for g in gs]
-    zs = torch.nn.utils.rnn.pad_sequence(zs, batch_first=True)
-    rs = torch.nn.utils.rnn.pad_sequence(rs, batch_first=True)
-    ys = torch.stack(ys, dim=0)
-    return zs.float(), rs.float(), ys.float(), masks.float()
+from qm9 import dataset
+from qm9 import utils as qm9_utils
 
 class Readout(torch.nn.Module):
     def __init__(
@@ -45,20 +35,13 @@ class Readout(torch.nn.Module):
         return h
 
 def run(args):
-    from dgl.data import QM9Dataset
-    data = QM9Dataset(label_keys=[args.key])
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    dtype = torch.float32
 
-    frac_list = [100000, 18000, 13000]
-    frac_list = [float(x) / sum(frac_list) for x in frac_list]
-
-    ds_tr, ds_vl, ds_te = dgl.data.utils.split_dataset(data, random_state=0, shuffle=True, frac_list=frac_list)
-
-    mu, sigma = data.label.mean(), data.label.std()
+    dataloaders, charge_scale = dataset.retrieve_dataloaders(args.batch_size, 4)
+    # compute mean and mean absolute deviation
+    mu, sigma = qm9_utils.compute_mean_mad(dataloaders, args.property)
     coloring = sake.Coloring(mu, sigma)
-
-    from torch.utils.data import DataLoader
-    ds_tr_loader = DataLoader(ds_tr, batch_size=96, collate_fn=padding, pin_memory=True)
-    ds_vl_loader = DataLoader(ds_vl, batch_size=96, collate_fn=padding, pin_memory=True)
 
     from sake.baselines.egnn import EGNNLayer
     model = sake.DenseSAKEModel(
@@ -72,7 +55,6 @@ def run(args):
     )
     readout = Readout(in_features=128, hidden_features=128, out_features=1)
 
-
     if torch.cuda.is_available():
         model = model.cuda()
         readout = readout.cuda()
@@ -85,52 +67,23 @@ def run(args):
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.n_epochs)
 
     for idx_epoch in range(args.n_epochs):
-        for z, r, y, mask in ds_tr_loader:
-            if torch.cuda.is_available():
-                z = z.cuda()
-                r = r.cuda()
-                y = y.cuda()
-                mask = mask.cuda()
-
-            optimizer.zero_grad()
-            y_hat, _ = model(z, r, mask=mask)
-            y_hat = readout(y_hat, mask=mask)
-            y_hat = coloring(y_hat)
-            loss = torch.nn.MSELoss()(y_hat, y)
-            loss.backward()
-            optimizer.step()
-        scheduler.step()
-        
-        with torch.no_grad():
-            model.eval()
-            ys = []
-            ys_hat = []
-
-            for z, r, y, mask in ds_vl_loader:
-                z = z.cuda().float()
-                r = r.cuda()
-                y = y.cuda()
-                mask = mask.cuda().float()
-
-                y_hat, _ = model(z, r, mask=mask)
-                y_hat = readout(y_hat, mask=mask)
-                y_hat = coloring(y_hat)
-
-                ys.append(y)
-                ys_hat.append(y_hat)
-
-            ys = torch.cat(ys, dim=0)
-            ys_hat = torch.cat(ys_hat, dim=0)
-            loss = torch.nn.L1Loss()(ys, ys_hat)
-            print(loss, flush=True)
-                
-
+        for data in dataloaders['train']:
+            batch_size, n_nodes, _ = data['positions'].size()
+            atom_positions = data['positions'].view(batch_size * n_nodes, -1).to(device, dtype)
+            atom_mask = data['atom_mask'].view(batch_size * n_nodes, -1).to(device, dtype)
+            edge_mask = data['edge_mask'].to(device, dtype)
+            one_hot = data['one_hot'].to(device, dtype)
+            charges = data['charges'].to(device, dtype)
+            nodes = qm9_utils.preprocess_input(one_hot, charges, args.charge_power, charge_scale, device)
+            print(nodes.shape)
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--key", type=str, default="U")
+    parser.add_argument("--property", type=str, default="U")
     parser.add_argument("--lr", type=float, default=1e-3)
     parser.add_argument("--n_epochs", type=int, default=1000)
+    parser.add_argument("--charge_power", type=int, default=2)
+    parser.add_argument("--batch_size", type=int, default=96)
     args = parser.parse_args()
     run(args)
