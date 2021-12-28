0e7ea68ad465c7e51bc94e7b9154dda151dd4e1e
yuanqing-wang <wangyq@wangyq.net>
2021-12-27 20:54:45 -0500

    make baseline a module

--- sake/__init__.py
+++ sake/__init__.py
@@ -1,4 +1,4 @@
-from . import layers, models, utils, functional
+from . import layers, models, utils, functional, baselines
 from .layers import GlobalSumSAKELayer, DenseSAKELayer, SparseSAKELayer
 from .utils import RBF, HardCutOff, Coloring, ConditionalColoring, ContinuousFilterConvolution, bootstrap, ConcatenationFilter
 from .models import DenseSAKEModel, SparseSAKEModel, TandemDenseSAKEModel
