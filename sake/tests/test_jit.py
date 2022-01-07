00b06ffd2b6823cc09140ab7280484da0fb21eea
Yuanqing Wang <wangyq@wangyq.net>
2022-01-07 13:34:04 -0500

    cuda jit

--- sake/tests/test_jit.py
+++ sake/tests/test_jit.py
@@ -1,10 +1,24 @@
 import pytest
 import numpy.testing as npt
+import torch
 
 def test_simple_forward(_equivariance_test_utils):
     import torch
     import sake
     h0, x0, _, __, ___ = _equivariance_test_utils
     layer = sake.DenseSAKELayer(7, 6, 5, residual=False)
+    layer = torch.jit.script(layer)
+    h, x = layer(h0, x0)
+
+
+@pytest.skipif(torch.cuda.is_available() is False, reason="no cuda")
+def test_simple_forward(_equivariance_test_utils):
+    import torch
+    import sake
+    h0, x0, _, __, ___ = _equivariance_test_utils
+    h0 = h0.cuda()
+    x0 = x0.cuda()
+    layer = sake.DenseSAKELayer(7, 6, 5, residual=False)
+    layer = layer.cuda()
     layer = torch.jit.script(layer)
     h, x = layer(h0, x0)
