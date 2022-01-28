76ccb851e46049d8dd86627ea07a0488eba0d086
yuanqing-wang <wangyq@wangyq.net>
2022-01-27 22:18:14 -0500

    dw4 scripts

---
+++ scripts/dw4/prep.py
@@ -0,0 +1,12 @@
+def run():
+    file_handle = open("en_flows/dw4_experiment/dataset.py", "r")
+    lines = file_handle.readlines()
+    lines = [line.replace("dw4_experiment/data/", "en_flows/dw4_experiment/data/") for line in lines]
+    file_handle.close()
+    file_handle = open("en_flows/dw4_experiment/dataset.py", "w")
+    file_handle.writelines(lines)
+    file_handle.close()
+
+
+if __name__ == "__main__":
+    run()
