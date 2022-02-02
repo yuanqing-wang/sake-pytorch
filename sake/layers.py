35880856772a11540bf12638b0031ed8b2d41f23
yuanqing-wang <wangyq@wangyq.net>
2022-02-02 17:38:36 -0500

    rbf

--- sake/layers.py
+++ sake/layers.py
@@ -92,7 +92,7 @@ class SAKELayer(torch.nn.Module):
         self.coefficients_mlp = torch.nn.Sequential(
             torch.nn.Linear(hidden_features, hidden_features),
             activation,
-            torch.nn.Linear(hidden_features, 3 * n_coefficients),
+            torch.nn.Linear(hidden_features, 4 * n_coefficients),
         )
 
         self.post_norm_mlp = torch.nn.Sequential(
@@ -109,8 +109,9 @@ class SAKELayer(torch.nn.Module):
 class DenseSAKELayer(SAKELayer):
     def spatial_attention(self, h_e_mtx, x_minus_xt, x_minus_xt_norm, mask: Union[None, torch.Tensor]=None):
         # (batch_size, n, n, coefficients, 1)
-        coefficients, alpha, beta = self.coefficients_mlp(h_e_mtx).unsqueeze(-1).split(self.n_coefficients, dim=-2)
+        coefficients, alpha, beta, lamb = self.coefficients_mlp(h_e_mtx).unsqueeze(-1).split(self.n_coefficients, dim=-2)
         alpha = alpha.exp()
+        lamb = lamb + 1.0
 
         # (batch_size, n, n, 3)
         x_minus_xt_direction = x_minus_xt / (x_minus_xt_norm + 1e-5)
